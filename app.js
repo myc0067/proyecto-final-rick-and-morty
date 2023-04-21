855bd6be791e5c6f17732ac5431bf011c2b0a0c5
myc0067 <ajahoulli@hotmail.com>
2023-04-20 23:26:48 -0300

    modifico app y css, y pruebo git clear

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const $container = document.getElementById("char-container")
 fetch("https://rickandmortyapi.com/api/character")
-.then ((response) => response.json())
-.then ((data) => {
-    const characters = data.results;
-    console.log(characters)
-    for (let character of characters) {
-        $container.innerHTML += `
-        <div class="card">
-            <img class="character-image" src=${character.image} 
-                alt="Imagen de ${character.name}">
-                <div class="character-info">
-                    <h3 class="character-name">${character.name}</h3>
-                    <p>Gender: ${character.gender}</p>
-                    <p>Species: ${character.species}</p>
-                    <p>Status: ${character.status}</p>
-                </di>
-        </div>
-        `}
-})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const personajes = data.results;
+    const $contenedor = document.getElementById("char-container");
+
+    console.log(personajes[0]);
+
+    for (let i = 0; i < personajes.length; i++) {
+      $contenedor.innerHTML += `
+      <div class="char">
+        <img class="char-img" src="${personajes[i].image}">
+        <p class="nombre-personaje">${personajes[i].name}</p>
+        <p>Gender: ${personajes[i].gender}</p>
+        <p>Species: ${personajes[i].species}</p>
+        <p>Status: ${personajes[i].status}</p>
+
+    
+      </div>
+      `;
+    }
+  });
